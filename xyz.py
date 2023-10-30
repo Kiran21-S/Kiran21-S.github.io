4021e04909f9f24bbc32db8e54f83320cbe81728
unknown <kiransindagi1@gmail.com>
2023-10-30 12:16:53 +0530

    xyz

---
+++ xyz.py
@@ -0,0 +1,19 @@
+# import math
+# radius = float(input("Enter radius: "))
+# area = math.pi*(radius**2)
+# print("Area of circle is : ",area)
+
+# side = float(input("Enter the length of side: "))
+# area= side**2
+# print("Area of sqaure is: ", area)
+
+# height = float(input("Enter height: "))
+# base = float(input("Enter base: "))
+# area = (0.5)*base*height
+# print("Area of triangle is: ", area)
+
+# principle_amt = float(input("Enter initial principle amount: "))
+# rate = float(input("Enter annual rate of interest: "))
+# time = float(input("Enter time period: "))
+# SI = (principle_amt*rate*time)/100
+# print("Simple interest is: ",SI)
